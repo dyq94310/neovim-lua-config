b134930386c78e6ae74a6d428fce0eb70ed7fbab
dyq <dyq94310@foxmail.com>
2023-03-08 15:18:06 +0800

    add lua lsp

--- lua/lsp/mason.lua
+++ lua/lsp/mason.lua
@@ -2,10 +2,12 @@ local mason = require("mason")
 
 local mason_lspconfig = require("mason-lspconfig")
 mason_lspconfig.setup({
-  ensure_installed = { "jdtls", "lua_ls" }
+  ensure_installed = { "jdtls", "lua_ls" },
+  automatic_installation = true,
 })
 
 require("lspconfig").jdtls.setup {}
+require("lspconfig").lua_ls.setup {}
 
 mason.setup({
   ui = {
