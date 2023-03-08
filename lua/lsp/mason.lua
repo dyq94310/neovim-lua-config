ad9dcf94ff1eeacacb1638a8fc08859f3dbf8805
dyq <dyq94310@foxmail.com>
2023-03-08 15:06:53 +0800

    update # Please enter the commit message for your changes. Lines starting

--- lua/lsp/mason.lua
+++ lua/lsp/mason.lua
@@ -1,18 +1,12 @@
-local status, mason = pcall(require, "mason")
-if not status then
-  vim.notify("没有找到 mason")
-  return
-end
+local mason = require("mason")
 
 local mason_lspconfig = require("mason-lspconfig")
-
 mason_lspconfig.setup({
   ensure_installed = { "jdtls", "lua_ls" }
 })
+
 require("lspconfig").jdtls.setup {}
--- require('jdtls').start_or_attach(config)
--- :h mason-default-settings
--- ~/.local/share/nvim/mason
+
 mason.setup({
   ui = {
     icons = {
